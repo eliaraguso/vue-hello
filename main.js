24d540064f5ab3e5cef10188b701fb96dc02860a
eliaraguso <elia.raguso@gmail.com>
2021-09-16 15:44:48 +0200

    Creata pagina Hello Vue

---
+++ main.js
@@ -0,0 +1,14 @@
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+
+var app = new Vue({
+
+    el: "#root",
+    data: {
+        title: "Hello Vue",
+        image: "https://lh3.googleusercontent.com/proxy/KOlZ0zwgllWIHhrPbgAQ0nGC1A4emoGr5utdQIM3vnV6BHmCYsCgvGqMdnV9HU5JG-KOfv5E8ZF2LdP4yaJDhahlRVK_MVlfygq__iE3sTvydCxqWujZ5gKoNtao-JuUTMeM7ofvD8y5EzKPqBKArKGYLTrefYS_wFASuXH_tfzchaZY_VXfPIt0LSJZLpcYOF8S"
+    }
+});
